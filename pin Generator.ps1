a559a0ed6ce05a31b1353cdd0828c6da9119e4a1
RowdyRooster <34560472+Rowdyrooster@users.noreply.github.com>
2017-12-14 20:46:15 -0600

    Add files via upload
    A base script for a random pin generator.

---
+++ pin Generator.ps1
@@ -0,0 +1,6 @@
+﻿
+Write-Host "Pin Generator..........."
+$chars = "0123456789".ToCharArray()
+$newpin = ""
+1..6 | ForEach { $newpin += $chars | Get-Random}
+Write-host $newpin -ForegroundColor "yellow"
