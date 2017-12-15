cb6961c96ffd38c9f0ab01d217fb27340ea55bed
RowdyRooster <34560472+Rowdyrooster@users.noreply.github.com>
2017-12-14 20:58:13 -0600

    Add files via upload
    Template for the switches inn the AD tool

---
+++ Switch template.ps1
@@ -0,0 +1,15 @@
+﻿Do {
+Write-Host "
+----------Active Directory Options----------
+1 = AD Pwassword reset
+2 = AD User disabled
+3 = AD Computer Disabled
+--------------------------"
+$choice1 = read-host -prompt "Select number & press enter"
+} until ($choice1 -eq "1" -or $choice1 -eq "2" -or $choice1 -eq "3")
+
+Switch ($choice1) {
+"1" {write next sub-menu here}
+"2" {write next sub-menu here}
+"3" {Write next sub-menu here}
+}
