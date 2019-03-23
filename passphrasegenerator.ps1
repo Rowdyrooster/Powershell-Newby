b62c7d694313795fa43547d611166e4efe284f12
RowdyRooster <34560472+Rowdyrooster@users.noreply.github.com>
2019-03-23 09:52:35 -0500

    Add files via upload

---
+++ passphrasegenerator.ps1
@@ -0,0 +1,11 @@
+﻿While($true){
+Write host - 'Randomizing PassPhrase....'
+$rand = new-object System.Random
+$conjunction = "the","my","we","our","and","but","+"
+$words = import-csv c:\dict.csv
+$word1 = ($words[$rand.Next(0,$words.Count)]).Word
+$con = ($conjunction[$rand.Next(0,$conjunction.Count)])
+$word2 = ($words[$rand.Next(0,$words.Count)]).Word
+return $word1 + " " + $con + " " + $word2
+Write-Host $word1,$con,$word2 -ForegroundColor Red
+}
