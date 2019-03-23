28c60f5ad1c62e5f4b0b0bac52013c57c5fb369a
RowdyRooster <34560472+Rowdyrooster@users.noreply.github.com>
2019-03-23 10:08:56 -0500

    Update passphrasegenerator.ps1

--- passphrasegenerator.ps1
+++ passphrasegenerator.ps1
@@ -1,11 +1,11 @@
-﻿While($true){
+While($true){
 Write host - 'Randomizing PassPhrase....'
-$rand = new-object System.Random
+$passph = new-object System.Random
 $conjunction = "the","my","we","our","and","but","+"
 $words = import-csv c:\dict.csv
-$word1 = ($words[$rand.Next(0,$words.Count)]).Word
-$con = ($conjunction[$rand.Next(0,$conjunction.Count)])
-$word2 = ($words[$rand.Next(0,$words.Count)]).Word
+$word1 = ($words[$passph.Next(0,$words.Count)]).Word
+$con = ($conjunction[$passph.Next(0,$conjunction.Count)])
+$word2 = ($words[$passph.Next(0,$words.Count)]).Word
 return $word1 + " " + $con + " " + $word2
 Write-Host $word1,$con,$word2 -ForegroundColor Red
 }
